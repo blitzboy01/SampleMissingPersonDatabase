ab2a2fef63c200dfd3564a113514501689fcc0a8
blitzboy01 <francoisebelmi@gmail.com>
2025-09-28 04:48:02 +0800

    Refactor ShoppingList component and remove unused files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-function MyButton(){
-  return <button>Click me</button>;
-}
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <MyButton />
-    </div>
+const products = [
+  {title: 'Cabbage',isFruit:false,id:1},
+  {title:'Garlic', isFruit:false,id:2},
+  {title:'Apple', isFruit:true,id:3}
+]
+
+export default function ShoppingList(){
+  const listItems = products.map(product =>
+    <li key={product.id}
+    style={{color:product.isFruit ?'magenta':'darkgreen'}}
+    >
+      {product.title}
+    </li>
   );
+
+  return(
+    <ul>{listItems}</ul>
+  );
+
 }
